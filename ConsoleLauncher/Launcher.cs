61b4b133963ba9daab7b7b43baa0ebe91ce6f294
Jakub Gramburg <jacubgr@gmail.com>
2022-04-25 20:44:31 +0200

    replaced layout/field approach with header class

--- ConsoleLauncher/Launcher.cs
+++ ConsoleLauncher/Launcher.cs
@@ -1,17 +1,12 @@
 ﻿namespace ConsoleLauncher
 {
+    using ConsoleLauncher.Layout;
+
     /// <summary>
     /// ConsoleLauncher tool.
     /// </summary>
     public class Launcher
     {
-        private static Layout? layout;
-
-        public static void SetLayout(Field header, Field footer)
-        {
-            layout = new Layout(header, footer);
-        }
-
         /// <summary>
         /// Initializes user-friendly, console menu that
         /// allow user to use arrows to navigate thru menu options.
@@ -19,48 +14,54 @@
         /// <param name="options">List of options to show in menu.</param>
         public static void Menu(List<Option> options)
         {
-            ConsoleKeyInfo keyAction;
-            int pointer = 0;
-
             if (options.Count < 1)
             {
                 throw new NotImplementedException("Option list contains no elements!");
             }
 
+            ConsoleKeyInfo keyAction;
+            int pointer = 0;
+
             do
             {
                 GenerateView(options, pointer);
 
-                keyAction = GetAction();
-
-                switch (keyAction.Key)
-                {
-                    case ConsoleKey.Enter:
-                        if (options[pointer].Action != null)
-                        {
-                            options[pointer].Action.Invoke();
-                        }
-
-                        break;
-                    case ConsoleKey.PageUp:
-                    case ConsoleKey.UpArrow:
-                        if (pointer > 0)
-                        {
-                            pointer--;
-                        }
-
-                        break;
-                    case ConsoleKey.PageDown:
-                    case ConsoleKey.DownArrow:
-                        if (pointer < options.Count - 1)
-                        {
-                            pointer++;
-                        }
-
-                        break;
-                }
+                keyAction = Console.ReadKey(true);
+                pointer = MakeAction(options, keyAction, pointer);
             }
             while (keyAction.Key != ConsoleKey.Escape);
+        }
+
+        private static int MakeAction(List<Option> options, ConsoleKeyInfo keyAction, int pointer)
+        {
+            switch (keyAction.Key)
+            {
+                case ConsoleKey.Enter:
+                    if (options[pointer].Action != null)
+                    {
+                        options[pointer].Action.Invoke();
+                    }
+
+                    break;
+                case ConsoleKey.PageUp:
+                case ConsoleKey.UpArrow:
+                    if (pointer > 0)
+                    {
+                        pointer--;
+                    }
+
+                    break;
+                case ConsoleKey.PageDown:
+                case ConsoleKey.DownArrow:
+                    if (pointer < options.Count - 1)
+                    {
+                        pointer++;
+                    }
+
+                    break;
+            }
+
+            return pointer;
         }
 
         /// <summary>
@@ -71,8 +72,17 @@
         private static void GenerateView(List<Option> options, int pointer)
         {
             Console.Clear();
-            Console.WriteLine();
-            PrintHeader();
+            Console.CursorVisible = false;
+            if (Header.HeaderVisible == true)
+            {
+                PrintHeader();
+            }
+
+            PrintBody(options, pointer);
+        }
+
+        private static void PrintBody(List<Option> options, int pointer)
+        {
             for (int i = 0; i < options.Count; i++)
             {
                 if (pointer == i)
@@ -92,15 +102,17 @@
 
         private static void PrintHeader()
         {
-            Console.Write((Char)13 + layout.Header.Left);
+            Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop);
+            Console.Write(Header.Title);
 
-            Console.SetCursorPosition(Console.WindowWidth - layout.Header.Right.Length, Console.CursorTop);
-            Console.Write(layout.Header.Right);
-        }
+            if (Header.ClockVisible)
+            {
+                var formattedTime = DateTime.Now.ToString(Header.TimeFormat);
+                Console.SetCursorPosition(Console.WindowWidth - formattedTime.Length, Console.CursorTop);
+                Console.Write(formattedTime);
+            }
 
-        private static ConsoleKeyInfo GetAction()
-        {
-            return Console.ReadKey(true);
+            Console.WriteLine();
         }
     }
 }
---
+++ ConsoleLauncher/Layout/Header.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleLauncher.Layout
+{
+    /// <summary>
+    /// Header used to print at top of the screen.
+    /// </summary>
+    public static class Header
+    {
+        private static readonly string ProcessName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+
+        static Header()
+        {
+            HeaderVisible = false;
+            Title = ProcessName;
+            ClockVisible = true;
+            TimeFormat = "h:mm tt";
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the header is visible.
+        /// </summary>
+        public static bool HeaderVisible { get; set; }
+
+        /// <summary>
+        /// Gets or sets customizable title. By default = project name.
+        /// </summary>
+        public static string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the clock is visible. Default = true.
+        /// </summary>
+        public static bool ClockVisible { get; set; }
+
+        /// <summary>
+        /// Gets or sets time display format. Default = "h:mm tt".
+        /// </summary>
+        public static string TimeFormat { get; set; }
+    }
+}
